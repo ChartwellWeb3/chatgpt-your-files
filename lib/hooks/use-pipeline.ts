03f23cfb8549800e405028b90cf5c6271f8d12c1
Chartwell Retirement Residences <38982347+ChartwellWeb3@users.noreply.github.com>
2025-10-03 14:34:33 -0400

    chore: Remove unnecessary blank line in usePipeline hook

--- lib/hooks/use-pipeline.ts
+++ lib/hooks/use-pipeline.ts
@@ -9,7 +9,6 @@ import {
 export type PipeParameters = Parameters<Pipeline['_call']>;
 export type PipeReturnType = Awaited<ReturnType<Pipeline['_call']>>;
 export type PipeFunction = (...args: PipeParameters) => Promise<PipeReturnType>;
-
 /**
  * Hook to build a Transformers.js pipeline function.
  *
